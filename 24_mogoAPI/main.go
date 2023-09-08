c63c5313fef010d4ba1f2a33d59f0e5cb7972396
Hemanth Kumar <pusupalahemanthkumar@gmail.com>
2023-09-08 22:41:35 +0530

    Day-5 Go Basics

---
+++ 24_mogoAPI/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	router "github/pusupalahemanthkumar/mongoAPI/routes"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("MongoDB API")
+	r := router.Router()
+	fmt.Println("Server is getting started...")
+	log.Fatal(http.ListenAndServe(":4000", r))
+	fmt.Println("Listening at port 4000 ...")
+}
